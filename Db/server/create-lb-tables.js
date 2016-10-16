54c86119c99bbd9e5f2181266c1207cdaf56c636
vallo <vallo61@gmail.com>
2016-10-16 18:54:41 -0300

    create

--- Db/server/create-lb-tables.js
+++ Db/server/create-lb-tables.js
@@ -1,7 +1,9 @@
 var server = require('./server');
+var loopback = require('loopback');
+var app = module.exports = loopback();
 var ds = server.dataSources.db;
 server.app.models.cliente.attachTo(app.dataSources.mysql);
-var lbTables = ['Cliente'];
+var lbTables = ['cliente'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
