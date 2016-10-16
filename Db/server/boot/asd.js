3351a03faea4eafd89219e2320fed50947a19790
vallo <vallo61@gmail.com>
2016-10-16 19:03:49 -0300

    ok

--- Db/server/boot/asd.js
+++ Db/server/boot/asd.js
@@ -1,15 +1,5 @@
 module.exports = function(app) {
-  app.dataSources.mysql.automigrate('Role', function(err) {
+  app.dataSources.mysql.automigrate('cliente', function(err) {
     if (err) console.log(err);
-  });
-  app.dataSources.mysql.automigrate('AccessToken', function(err) {
-    if (err) console.log(err);
-  });
-  app.dataSources.mysql.automigrate('ACL', function(err) {
-    if (err) console.log(err);
-  });
-  app.dataSources.mysql.automigrate('RoleMapping', function(err) {
-    if (err) console.log(err);
-  });
-  
+  });  
 };
