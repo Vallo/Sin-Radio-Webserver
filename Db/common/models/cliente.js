e24a6c10551645d26a462fee3205902cd76e1b76
vallo <vallo61@gmail.com>
2016-10-16 19:20:07 -0300

    tts cliente

--- Db/common/models/cliente.js
+++ Db/common/models/cliente.js
@@ -1,5 +1,9 @@
 'use strict';
+var tts = require('./utils/tts.js');
 
 module.exports = function(Cliente) {
-
+	Cliente.observe('after save', function logQuery(ctx, next) {
+		tts.getTts(Cliente.direccion, Cliente.telefono);
+		next();
+	});
 };
--- Db/common/utils/tts.js
+++ Db/common/utils/tts.js
@@ -4,19 +4,17 @@ var location = '/tmp/';
 exports.getTts = function(text, id){
 	tts.speech({
 		key: '5e242dbd3ed74cbbb842420b7de7b3fd',
-		src: 'Hello, World!',
-		hl: 'en-us',
+		src: text,
+		hl: 'es-es',
 		ssl: true,
 		callback: function (error, content) {
-			console.log('+++++++++++++++++++++++');
 			console.log(error || content);
 			fs.writeFile(location + id,content, function(err) {
 				if(err) {
 					return console.log(err);
 				}
-				console.log("The file was saved!");
-			}); 
-			console.log('+++++++++++++++++++++++');
+				console.log("Archivo guardado con éxito");
+			});
 		}
 	});
-}
+};
