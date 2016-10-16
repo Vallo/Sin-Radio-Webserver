033db4665e9cba5d8aaea52f90cff591ab1ec44c
vallo <vallo61@gmail.com>
2016-10-16 19:28:12 -0300

    harto

--- Db/common/models/cliente.js
+++ Db/common/models/cliente.js
@@ -3,8 +3,8 @@ var tts = require('../utils/tts.js');
 
 module.exports = function(Cliente) {
 	Cliente.observe('after save', function logQuery(ctx, next) {
-		console.log(Cliente.direccionFavorita);
-		tts.getTts(Cliente.direccionFavorita, Cliente.telefono);
+		console.log(ctx.instance.direccionFavorita);
+		tts.getTts(ctx.instance.direccionFavorita, Cliente.telefono);
 		next();
 	});
 };
