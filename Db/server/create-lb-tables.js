dc46346a99b16b8da17232b48ddee6e1ea233c17
vallo <vallo61@gmail.com>
2016-10-16 18:51:13 -0300

    asd

--- Db/server/create-lb-tables.js
+++ Db/server/create-lb-tables.js
@@ -1,5 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.db;
+server.app.models.cliente.attachTo(app.dataSources.mysql);
 var lbTables = ['Cliente'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
