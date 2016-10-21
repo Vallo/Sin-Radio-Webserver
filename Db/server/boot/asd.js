1b41feed8bdf6740acefd5cff1d06ba3d8c78b03
root <root@vallo.vallo>
2016-10-20 23:08:53 -0300

    fix init

--- Db/server/boot/asd.js
+++ Db/server/boot/asd.js
@@ -1,5 +1,20 @@
 module.exports = function(app) {
-  app.dataSources.mysql.automigrate('chofer,cliente,usuario,movil,denuncia,viaje', function(err) {
+  app.dataSources.mysql.automigrate('chofer', function(err) {
+    if (err) console.log(err);
+  });
+  app.dataSources.mysql.automigrate('cliente', function(err) {
     if (err) console.log(err);
   });  
+  app.dataSources.mysql.automigrate('usuario', function(err) {
+    if (err) console.log(err);
+  });  
+  app.dataSources.mysql.automigrate('movil', function(err) {
+    if (err) console.log(err);
+  });  
+  app.dataSources.mysql.automigrate('denuncia', function(err) {
+    if (err) console.log(err);
+  });  
+  app.dataSources.mysql.automigrate('viaje', function(err) {
+    if (err) console.log(err);
+  });
 };
