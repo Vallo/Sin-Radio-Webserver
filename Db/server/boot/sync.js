75d69ddf517b2213be20bdcb4ceb4b12d744eb40
root <root@vallo.vallo>
2016-10-20 22:19:55 -0300

    saco el cron por el momento

--- Db/server/boot/sync.js
+++ Db/server/boot/sync.js
@@ -22,14 +22,14 @@ function updateViajes() {
 }*/
 
 function updateChoferes() {
-	db.query('SELECT 1 as ANDROID_ID FROM CHOFER').then(function(res){
+	/*db.query('SELECT 1 as ANDROID_ID FROM chofer').then(function(res){
   		requestify.get('http://api.sin-radio.com.ar/chofer/').then(function(response){
   			var choferes = JSON.parse(response.body);
-  			/*for (var i = 0; i < choferes.length; i++){
+  			for (var i = 0; i < choferes.length; i++){
   				//console.log(choferes[i]);
   				db.query('INSERT INTO CHOFER (ANDROID_ID, TEL,NOMBRE) VALUES (?,?,?)', [choferes[i].android_id, choferes[i].tel,choferes[i].nombre]);
   			};
-  			console.log('OUT');*/
+  			console.log('OUT');
 		});
-	});
+	});*/
 }
