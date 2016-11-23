92ec75a706b337607689d3b3ad7e438753898cba
Vallo <vallo61@gmail.com>
2016-11-23 01:21:43 -0300

    fullInit

--- Db/server/boot/init.js
+++ Db/server/boot/init.js
@@ -1,32 +1,41 @@
+var requestify = require('requestify')
+var url = require('../../config.js').cloud;
 module.exports = function(app) {
   if (process.argv[2] == 'init')
   {
-    app.dataSources.mysql.automigrate('chofer', function(err) {
-      if (err) console.log(err);
-    });
-    app.dataSources.mysql.automigrate('cliente', function(err) {
-      if (err) console.log(err);
-    });  
-    app.dataSources.mysql.automigrate('usuario', function(err) {
-      if (err) console.log(err);
-    });  
-    app.dataSources.mysql.automigrate('movil', function(err) {
-      if (err) console.log(err);
-    });  
-    app.dataSources.mysql.automigrate('denuncia', function(err) {
-      if (err) console.log(err);
-    }); 
-    app.dataSources.mysql.automigrate('emergencia', function(err) {
-      if (err) console.log(err);
-    }); 
-    app.dataSources.mysql.automigrate('posicion', function(err) {
-      if (err) console.log(err);
-    });
-    app.dataSources.mysql.automigrate('alertaVial', function(err) {
-      if (err) console.log(err)
-    });
-    app.dataSources.mysql.automigrate('viaje', function(err) {
-      if (err) console.log(err)
-    });
+    Init();
   }
-};
+  if (process.argv[2 == 'fullInit'])
+
+    requestify.get(url + 'Init');
+    function Init(){
+     app.dataSources.mysql.automigrate('chofer', function(err) {
+      if (err) console.log(err);
+    });
+     app.dataSources.mysql.automigrate('cliente', function(err) {
+      if (err) console.log(err);
+    });  
+     app.dataSources.mysql.automigrate('usuario', function(err) {
+      if (err) console.log(err);
+    });  
+     app.dataSources.mysql.automigrate('movil', function(err) {
+      if (err) console.log(err);
+    });  
+     app.dataSources.mysql.automigrate('denuncia', function(err) {
+      if (err) console.log(err);
+    }); 
+     app.dataSources.mysql.automigrate('emergencia', function(err) {
+      if (err) console.log(err);
+    }); 
+     app.dataSources.mysql.automigrate('posicion', function(err) {
+      if (err) console.log(err);
+    });
+     app.dataSources.mysql.automigrate('alertaVial', function(err) {
+      if (err) console.log(err)
+    });
+     app.dataSources.mysql.automigrate('viaje', function(err) {
+      if (err) console.log(err)
+    });
+   }
+ };
+
