69690f9d94e093e58dc509d7d66e653345c669c6
Vallo <vallo61@gmail.com>
2016-11-23 01:16:05 -0300

    ahora con parametro init inicializo la db

--- Db/server/boot/init.js
+++ Db/server/boot/init.js
@@ -1,31 +1,32 @@
 module.exports = function(app) {
-/*  
-  app.dataSources.mysql.automigrate('chofer', function(err) {
-    if (err) console.log(err);
-  });
-  app.dataSources.mysql.automigrate('cliente', function(err) {
-    if (err) console.log(err);
-  });  
-  app.dataSources.mysql.automigrate('usuario', function(err) {
-    if (err) console.log(err);
-  });  
-  app.dataSources.mysql.automigrate('movil', function(err) {
-    if (err) console.log(err);
-  });  
-  app.dataSources.mysql.automigrate('denuncia', function(err) {
-    if (err) console.log(err);
-  }); 
-  app.dataSources.mysql.automigrate('emergencia', function(err) {
-    if (err) console.log(err);
-  }); 
-  app.dataSources.mysql.automigrate('posicion', function(err) {
-    if (err) console.log(err);
-  });
-  app.dataSources.mysql.automigrate('alertaVial', function(err) {
-    if (err) console.log(err)
-  });
-  app.dataSources.mysql.automigrate('viaje', function(err) {
-    if (err) console.log(err)
-  });
-*/
+  if (process.argv[2] == 'init')
+  {
+    app.dataSources.mysql.automigrate('chofer', function(err) {
+      if (err) console.log(err);
+    });
+    app.dataSources.mysql.automigrate('cliente', function(err) {
+      if (err) console.log(err);
+    });  
+    app.dataSources.mysql.automigrate('usuario', function(err) {
+      if (err) console.log(err);
+    });  
+    app.dataSources.mysql.automigrate('movil', function(err) {
+      if (err) console.log(err);
+    });  
+    app.dataSources.mysql.automigrate('denuncia', function(err) {
+      if (err) console.log(err);
+    }); 
+    app.dataSources.mysql.automigrate('emergencia', function(err) {
+      if (err) console.log(err);
+    }); 
+    app.dataSources.mysql.automigrate('posicion', function(err) {
+      if (err) console.log(err);
+    });
+    app.dataSources.mysql.automigrate('alertaVial', function(err) {
+      if (err) console.log(err)
+    });
+    app.dataSources.mysql.automigrate('viaje', function(err) {
+      if (err) console.log(err)
+    });
+  }
 };
